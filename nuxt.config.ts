58e59ea4437343328f81c716f0d9b80c5d27ba08
MWaseemAbbasi <mwaseem@tekambi.com>
2023-12-10 02:30:28 +0500

    SEO things done.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,11 +24,10 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'A awesome developer portfolio design.' },
+        { hid: 'description', name: 'description', content: 'Wasim | Waseem | Developer | Certified | VueJs | Laravel | Yii | React | M. Wasim Abbasi | GraphQL | REST.' },
         { hid: 'og:title', property: 'og:title', content: siteTitle },
-        { hid: 'og:description', property: 'og:description', content: 'A awesome developer portfolio design.' },
+        { hid: 'og:description', property: 'og:description', content: 'A professional & certified full-stack developer.' },
         { hid: 'og:image', property: 'og:image', content: 'demo-share.jpg' },
-        { hid: 'og:url', property: 'og:url', content: 'https://developer-portfolio-v1.netlify.app/' },
         { name: 'theme-color', content: '#010C15' },
         // ...
       ],
